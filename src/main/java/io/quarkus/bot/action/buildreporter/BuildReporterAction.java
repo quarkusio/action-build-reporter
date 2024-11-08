cca4614e7e684021f4b9fa0f6ff44eabafb20786
Guillaume Smet <guillaume.smet@gmail.com>
2024-11-08 19:11:32 +0100

    Prepare Quarkus LangChain4j arrival

--- src/main/java/io/quarkus/bot/action/buildreporter/BuildReporterAction.java
+++ src/main/java/io/quarkus/bot/action/buildreporter/BuildReporterAction.java
@@ -17,7 +17,6 @@ import io.quarkiverse.githubaction.Action;
 import io.quarkiverse.githubaction.Commands;
 import io.quarkiverse.githubaction.Context;
 import io.quarkiverse.githubaction.Inputs;
-import io.quarkus.bot.action.buildreporter.workflow.QuarkusWorkflowConstants;
 import io.quarkus.bot.buildreporter.githubactions.BuildReporterActionHandler;
 import io.quarkus.bot.buildreporter.githubactions.BuildReporterConfig;
 
@@ -50,10 +49,24 @@ public class BuildReporterAction {
             return;
         }
 
+        Comparator<GHWorkflowJob> workflowJobComparator;
+
+        switch (context.getGitHubRepository()) {
+            case "quarkusio/quarkus":
+                workflowJobComparator = QuarkusWorkflowJobComparator.INSTANCE;
+                break;
+            case "quarkiverse/quarkus-langchain4j":
+                workflowJobComparator = QuarkusLangChain4jWorkflowJobComparator.INSTANCE;
+                break;
+            default:
+                // it will use the default ones
+                workflowJobComparator = null;
+        }
+
         BuildReporterConfig buildReporterConfig = new BuildReporterConfig.Builder()
                 // only create check run and annotation if this is a fork
                 .createCheckRun(workflowRun.getRepository().isFork())
-                .workflowJobComparator(QuarkusWorkflowJobComparator.INSTANCE)
+                .workflowJobComparator(workflowJobComparator)
                 .build();
 
         Optional<String> report = buildReporterActionHandler.generateReport(context.getGitHubWorkflow(), workflowRun,
@@ -72,16 +85,101 @@ public class BuildReporterAction {
 
         @Override
         public int compare(GHWorkflowJob o1, GHWorkflowJob o2) {
-            if (o1.getName().startsWith(QuarkusWorkflowConstants.JOB_NAME_INITIAL_JDK_PREFIX)
-                    && !o2.getName().startsWith(QuarkusWorkflowConstants.JOB_NAME_INITIAL_JDK_PREFIX)) {
-                return -1;
-            }
-            if (!o1.getName().startsWith(QuarkusWorkflowConstants.JOB_NAME_INITIAL_JDK_PREFIX)
-                    && o2.getName().startsWith(QuarkusWorkflowConstants.JOB_NAME_INITIAL_JDK_PREFIX)) {
-                return 1;
+            int order1 = getOrder(o1.getName());
+            int order2 = getOrder(o2.getName());
+
+            if (order1 == order2) {
+                return o1.getName().compareToIgnoreCase(o2.getName());
             }
 
-            return o1.getName().compareTo(o2.getName());
+            return order1 - order2;
+        }
+
+        private static int getOrder(String jobName) {
+            if (jobName.startsWith("Initial JDK")) {
+                return 1;
+            }
+            if (jobName.startsWith("Calculate Test Jobs")) {
+                return 2;
+            }
+            if (jobName.startsWith("JVM Tests - ")) {
+                if (jobName.contains("Windows")) {
+                    return 12;
+                }
+                return 11;
+            }
+            if (jobName.startsWith("Maven Tests - ")) {
+                if (jobName.contains("Windows")) {
+                    return 22;
+                }
+                return 21;
+            }
+            if (jobName.startsWith("Gradle Tests - ")) {
+                if (jobName.contains("Windows")) {
+                    return 32;
+                }
+                return 31;
+            }
+            if (jobName.startsWith("Devtools Tests - ")) {
+                if (jobName.contains("Windows")) {
+                    return 42;
+                }
+                return 41;
+            }
+            if (jobName.startsWith("Kubernetes Tests - ")) {
+                if (jobName.contains("Windows")) {
+                    return 52;
+                }
+                return 51;
+            }
+            if (jobName.startsWith("Quickstarts Compilation")) {
+                return 61;
+            }
+            if (jobName.startsWith("MicroProfile TCKs Tests")) {
+                return 71;
+            }
+            if (jobName.startsWith("Native Tests - ")) {
+                if (jobName.contains("Windows")) {
+                    return 82;
+                }
+                return 81;
+            }
+
+            return 200;
+        }
+    }
+
+    private final static class QuarkusLangChain4jWorkflowJobComparator implements Comparator<GHWorkflowJob> {
+
+        private static final QuarkusLangChain4jWorkflowJobComparator INSTANCE = new QuarkusLangChain4jWorkflowJobComparator();
+
+        @Override
+        public int compare(GHWorkflowJob o1, GHWorkflowJob o2) {
+            int order1 = getOrder(o1.getName());
+            int order2 = getOrder(o2.getName());
+
+            if (order1 == order2) {
+                return o1.getName().compareToIgnoreCase(o2.getName());
+            }
+
+            return order1 - order2;
+        }
+
+        private static int getOrder(String jobName) {
+            if (jobName.startsWith("Quick Build")) {
+                return 1;
+            }
+            if (jobName.startsWith("JVM tests - ")) {
+                return 2;
+            }
+            if (jobName.startsWith("Native tests - ")) {
+                return 12;
+            }
+            if (jobName.startsWith("In process embedding model tests - ")) {
+                return 22;
+            }
+
+            return 200;
         }
     }
 }
